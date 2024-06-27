d9ca3ae4faec4b2503d8d6b6153e0b51a2b8d7a6
marvelKitu <131355973+marvelKitu@users.noreply.github.com>
2024-06-27 17:26:24 +0530

    Add files via upload

---
+++ 1-Landing Page/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('auth-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sign Up form submitted!');
+});
+
+document.getElementById('login-button').addEventListener('click', function() {
+    alert('Login button clicked!');
+});
